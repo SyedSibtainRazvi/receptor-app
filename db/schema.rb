a0db840ed7f6322dc9dedcdc048fba488be27e52
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-07-12 12:17:55 +0530

    chore: added rubocop for lint

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 20_240_712_112_956) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
