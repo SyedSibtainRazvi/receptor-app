37dcbf794d010861ab116724948d8042f431cef4
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-07-15 11:39:53 +0530

    added rubocop and fixed lint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in successfully!'
     else
-      flash.now[:alert] = "Invalid email or password. Please try again."
+      flash.now[:alert] = 'Invalid email or password. Please try again.'
       render :new, status: :unprocessable_entity
     end
   end
@@ -17,5 +18,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path, notice: 'Logged out successfully'
   end
-  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
-    def logged_in?
-        begin
-            User.find(session[:user_id]) if session[:user_id]
-        rescue ActiveRecord::RecordNotFound
-            nil
-        end
-    end
+  def logged_in?
+    User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
 end
