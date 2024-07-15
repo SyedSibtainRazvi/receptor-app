21ad1737ee0f38006ca9a9fbb1fc7979832b8b49
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-07-15 11:47:52 +0530

    chore: remove comments by rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module ApplicationHelper
   def logged_in?
     User.find(session[:user_id]) if session[:user_id]
--- db/migrate/20240712112956_add_unique_index_to_users_email.rb
+++ db/migrate/20240712112956_add_unique_index_to_users_email.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddUniqueIndexToUsersEmail < ActiveRecord::Migration[7.1]
   def change
     add_index :users, :email, unique: true
